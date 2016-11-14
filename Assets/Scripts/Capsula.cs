b3e1a8deefcdf3c623537a7a3c6ae53b3d302c80
Hiigaaran <simo.moralesg@gmail.com>
2016-11-13 23:38:32 -0300

    [IglesiaQuiz][ADD] TemplateQuiz/Assets/Scenes: se arregla reproducción de videos en escena capsula

--- Assets/Scripts/Capsula.cs
+++ Assets/Scripts/Capsula.cs
@@ -23,54 +23,71 @@ public class Capsula : MonoBehaviour {
     [SerializeField]
     private DificultdadManager dificultad;
 
+	[SerializeField]
+	private RawImage plano;
+
 	// Use this for initialization
 	void Start () {
 		DificultdadManager rescateDificultad = dificultad;
 		switch (rescateDificultad.getSacramentoActual().getId()) {
 		case 0:
-			GetComponent<RawImage> ().texture = sacramento1 as MovieTexture;
+			//GetComponent<RawImage> ().texture = sacramento1 as MovieTexture;
+			//GetComponent<AudioSource> ().clip = sacramento1.audioClip;
+			plano.texture = sacramento1 as MovieTexture;
 			GetComponent<AudioSource> ().clip = sacramento1.audioClip;
 
 			sacramento1.Play ();
 			GetComponent<AudioSource> ().Play ();
 			break;
 		case 1:
-			GetComponent<RawImage> ().texture = sacramento2 as MovieTexture;
+			//GetComponent<RawImage> ().texture = sacramento2 as MovieTexture;
+			//GetComponent<AudioSource> ().clip = sacramento2.audioClip;
+			plano.texture = sacramento2 as MovieTexture;
 			GetComponent<AudioSource> ().clip = sacramento2.audioClip;
 
 			sacramento2.Play ();
 			GetComponent<AudioSource> ().Play ();
 			break;
 		case 2:
-			GetComponent<RawImage> ().texture = sacramento3 as MovieTexture;
+			//GetComponent<RawImage> ().texture = sacramento3 as MovieTexture;
+			//GetComponent<AudioSource> ().clip = sacramento3.audioClip;
+			plano.texture = sacramento3 as MovieTexture;
 			GetComponent<AudioSource> ().clip = sacramento3.audioClip;
 
 			sacramento3.Play ();
 			GetComponent<AudioSource> ().Play ();
 			break;
 		case 3:
-			GetComponent<RawImage> ().texture = sacramento4 as MovieTexture;
+			//GetComponent<RawImage> ().texture = sacramento4 as MovieTexture;
+			//GetComponent<AudioSource> ().clip = sacramento4.audioClip;
+			plano.texture = sacramento4 as MovieTexture;
 			GetComponent<AudioSource> ().clip = sacramento4.audioClip;
 
 			sacramento4.Play ();
 			GetComponent<AudioSource> ().Play ();
 			break;
 		case 4:
-			GetComponent<RawImage> ().texture = sacramento5 as MovieTexture;
+			//GetComponent<RawImage> ().texture = sacramento5 as MovieTexture;
+			//GetComponent<AudioSource> ().clip = sacramento5.audioClip;
+			plano.texture = sacramento5 as MovieTexture;
 			GetComponent<AudioSource> ().clip = sacramento5.audioClip;
 
 			sacramento5.Play ();
 			GetComponent<AudioSource> ().Play ();
 			break;
 		case 5:
-			GetComponent<RawImage> ().texture = sacramento6 as MovieTexture;
+			//GetComponent<RawImage> ().texture = sacramento6 as MovieTexture;
+			//GetComponent<AudioSource> ().clip = sacramento6.audioClip;
+			plano.texture = sacramento6 as MovieTexture;
 			GetComponent<AudioSource> ().clip = sacramento6.audioClip;
 
 			sacramento6.Play ();
 			GetComponent<AudioSource> ().Play ();
 			break;
 		case 6:
-			GetComponent<RawImage> ().texture = sacramento7 as MovieTexture;
+			//GetComponent<RawImage> ().texture = sacramento7 as MovieTexture;
+			//GetComponent<AudioSource> ().clip = sacramento7.audioClip;
+			plano.texture = sacramento7 as MovieTexture;
 			GetComponent<AudioSource> ().clip = sacramento7.audioClip;
 
 			sacramento7.Play ();
